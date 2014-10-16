3d98200a0f68c9ec6956a5221988e18c622bb42b
Zackery Leman <zleman1593@gmail.com>
2014-10-15 22:48:17 -0400

    ccommented

--- Min-Conflicts-N-Queens-Problem/algorithms.swift
+++ Min-Conflicts-N-Queens-Problem/algorithms.swift
@@ -26,6 +26,7 @@ class minConflicts {
         self.conflicts = initialConflictCounter()
     }
     
+    
     func minConflicts()-> Bool{
         println("Current Random Assignment" + columns.description)
         println("Current Conflicts" + self.conflicts.description)
@@ -35,13 +36,14 @@ class minConflicts {
             if solution(){
                 return true
             }
+            //selects a random column
             let column =  Int(arc4random_uniform(UInt32(n)))
-            
+            //sets the queen in the selected column to the row that creates the fewest conflicts
             self.columns[column] = conflicts(column)
-            
             
         }
         
+        //return that it had to give up
         return false
     }
     
@@ -55,7 +57,7 @@ class minConflicts {
         var minConflicts = Int.max
         
         
-        for var columnAndRows = 0; columnAndRows < self.columns.count; ++columnAndRows {
+        for var row = 0; row < self.columns.count; ++columnAndRows {
             var  currentMoveConflicts = 0
             for var index = 0; index < self.columns.count; ++index {
                 
@@ -77,23 +79,27 @@ class minConflicts {
                 }
                 
             }
-            if columnAndRows == self.columns[variable]{
+            /*If the row being looked at is the row that the queen in the current column  currently occupies
+            then set currentPositionConflicts to the number of conflicts this queen is curently involved in.
+            */
+            if row == self.columns[variable]{
                 currentPositionConflicts = currentMoveConflicts
                 
-            }else if minConflicts >= currentMoveConflicts{
+            }/*Else if the row being looked at does not have the queen from the current column,
+            *update the best move if the move would result in fewer conflicts*/
+            else if minConflicts >= currentMoveConflicts{
                 minConflicts = currentMoveConflicts
                 bestMove = columnAndRows
             }
             
         }
         
-        //Keeps the number of conflicts updated
+        //Keeps the number of conflicts updated after a move is made
         if bestMove != self.columns[variable]{
             self.conflicts = self.conflicts + (minConflicts - currentPositionConflicts)
         }
-        
+        //Returns new row for queen to occupy that creates the fewest number of conflicts
         return bestMove
-        
     }
     
     
@@ -101,21 +107,22 @@ class minConflicts {
     func solution()-> Bool{
         if self.conflicts == 0 {
             return true
-            
         }
-        
         return false
-        
     }
+    
     //TODO MultiThread this?
+    /*Counts the number of conflicts created from the initial random assigmnet
+   *so that the number of conflicts can quickly updated during runtime*/
     func initialConflictCounter()-> Int{
         var totalConflicts = 0
         for var index = 0; index < self.columns.count; ++index {
             for var nextIndex = index+1; nextIndex < self.columns.count; ++nextIndex {
-                
+                //Looks across row
                 if  self.columns[index] == self.columns[nextIndex] {
                     totalConflicts++
                 }
+                //Looks at up and down diagnals
                 if self.columns[nextIndex] ==  (self.columns[index] + (nextIndex-index)){
                     totalConflicts++
                 }
