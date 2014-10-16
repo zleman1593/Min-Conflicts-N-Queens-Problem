390f598d57fde404f0a8f880afac3b41d10e6636
Zackery Leman <zleman1593@gmail.com>
2014-10-16 12:49:27 -0400

    made board scale

--- Min-Conflicts-N-Queens-Problem/ViewController.swift
+++ Min-Conflicts-N-Queens-Problem/ViewController.swift
@@ -11,7 +11,8 @@ import UIKit
 class ViewController: UIViewController, BoardDelegate {
     var solver : minConflicts!
     @IBOutlet var board : Board!
-    let DIMENSION: Int = 13
+    let DIMENSION: Int = 20
+    let SCALE_FACTOR: Int = 720
     override func viewDidLoad() {
         super.viewDidLoad()
         //sets self as the view's delegate
@@ -23,8 +24,10 @@ class ViewController: UIViewController, BoardDelegate {
         
         //Assigns detector to the view
         //self.board.addGestureRecognizer(tap)
+        
           self.board.boardHeight = DIMENSION
         self.board.boardWidth = DIMENSION
+        self.board.cellSize = SCALE_FACTOR/DIMENSION
         solver = minConflicts(n: DIMENSION, maxSteps:5000)
         //Update Board with starting Positions
         self.board.setNeedsDisplay()
