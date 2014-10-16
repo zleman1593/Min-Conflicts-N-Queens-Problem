78873dc114fa8f5710526aff2f09de53d4301193
Zackery Leman <zleman1593@gmail.com>
2014-10-15 21:32:31 -0400

    counts number of starting conflicts

--- Min-Conflicts-N-Queens-Problem/algorithms.swift
+++ Min-Conflicts-N-Queens-Problem/algorithms.swift
@@ -12,36 +12,44 @@ class minConflicts {
     var columns:Array<Int>!
     let n:Int!
     let maxSteps:Int!
+var conflicts:Int!
     
     init(n:Int, maxSteps:Int) {
         self.n = n
         self.maxSteps = maxSteps
-        
+
         columns = [Int(arc4random_uniform(UInt32(n)))]
         for var index = 1; index < n; ++index {
             let random = Int(arc4random_uniform(UInt32(n)))
             columns.append(random)
         }
+self.conflicts = initialConflictCounter()
     }
     
     func minConflicts()-> Bool{
-        println(columns.description)
+        println("Current Random Assignment" + columns.description)
         
+         println("Current Conflicts" + self.conflicts.description)
+     
+        for var index = 0; index < self.maxSteps; ++index {
+            //First check if current assignment is solution
+            if solution(){
+                return true
+            }
+            
+            let column =  Int(arc4random_uniform(UInt32(n)))
+            
+            self.columns[column] = conflicts(column)
+            
+            
+        }
         
-     for var index = 0; index < self.maxSteps; ++index {
-    let column =  Int(arc4random_uniform(UInt32(n)))
-        
-        columns[column] = conflicts(column)
-        
-    
-    }
-        
-         return false
+        return false
     }
     
     
     func conflicts(variable:Int)-> Int{
-        
+
         
         return 4
         
@@ -49,4 +57,51 @@ class minConflicts {
     
     
     
+    func solution()-> Bool{
+              if self.conflicts == 0 {
+            return true
+
+                    }
+        
+        return false
+        
+    }
+
+func initialConflictCounter()-> Int{
+            var totalConflicts = 0
+ for var index = 0; index < self.columns.count; ++index {
+    
+    
+           for var nextIndex = index+1; nextIndex < self.columns.count; ++nextIndex {
+
+            
+            if  self.columns[index] == self.columns[nextIndex] {
+                totalConflicts++
+            }
+            if self.columns[nextIndex] ==  (self.columns[index] + (nextIndex-index)){
+                totalConflicts++
+            }
+            if self.columns[nextIndex] ==  (self.columns[index] - (nextIndex-index)){
+                totalConflicts++
+            }
+    }
 }
+    
+ 
+        return totalConflicts
+    }
+    
+    
+}
+//
+
+
+//and accumulate positions
+
+
+
+//Check diagnlos
+
+//What about dynamic programming
+
+//Multi Threading go in each direction and diagnal
